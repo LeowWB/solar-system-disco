4f8c004468871a9c9e38fd2016c1250d4b9c9253
Leow Wen Bin <41581512+LeowWB@users.noreply.github.com>
2019-10-14 12:21:22 +0800

    Modify batch file to reflect new file name

--- a.bat
+++ a.bat
@@ -1,5 +1,5 @@
 del output.txt
 cls
-powershell -Command "Measure-Command {python sudoku_A2_xx.py input.txt output.txt | Out-Default}"
+powershell -Command "Measure-Command {python sudoku_A2_42.py input.txt output.txt | Out-Default}"
 fc output.txt expected.txt
 
